fc42c8c7f8bb0914c2df2bb7786ffeb644eaa6ef
unknown <kinchoily@hotmail.com>
2021-11-11 18:34:05 +0000

    AllQuotes-js - output QuoteList component

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,27 @@
+import { Fragment } from 'react';
+import QuoteList from '../components/quotes/QuoteList';
 
-const AllQuotes = () => {
-  return <h1>Quotes Page</h1>;
+const AllQuotes = (props) => {
+
+  // replace with redux
+  // const DummyQuotes = [
+  //   {
+  //     id: "p1",
+  //     author: "Sarah",
+  //     text: "Learn React With React",
+  //   },
+  //   {
+  //     id: 'p2',
+  //     author: 'Emily',
+  //     text: 'Learn With Udemy'
+  //   }
+  // ];
+
+  return (
+    <Fragment>
+      <QuoteList quotes={props.dummyData} />
+    </Fragment>
+  );
 };
 
 export default AllQuotes;

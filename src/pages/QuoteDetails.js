fc5ac7b482ea37a6125f4ef95f70f8f7f4c93015
unknown <kinchoily@hotmail.com>
2021-11-11 18:39:18 +0000

    QuoteDetails.js - handle output of selected quote from a list of quotes

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,16 +1,26 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
-const QuoteDetails = () => {
+const QuoteDetails = (props) => {
 
   const params = useParams();
 
+  //find() – search through all the child elements only.
+  //filter() – search through all the elements.
+  const quote = props.quoteData.find(quote => quote.id === params.quoteId);
+  
+  if(!quote){
+    return <p>No Quote Found</p>
+  };
+
+  console.log(quote);
+
   return (
     <Fragment>
-      <h1>Quote Details page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text}/>
       <Route path="/quotes/:quoteId/comments">
         <Comments />
       </Route>

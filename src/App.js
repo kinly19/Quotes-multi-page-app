6c9ca38705bf34138be2a3a1fa54282711419f7b
unknown <kinchoily@hotmail.com>
2021-11-11 13:49:24 +0000

    App.js - use wrapper component Layout

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import { Fragment } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import AllQuotes from './pages/AllQuotes';
 import QuoteDetails from './pages/QuoteDetails';
 import AddQuotes from './pages/AddQuotes';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 //notes - dont forget / before the pathname
 
 function App() {
   return (
-    <Fragment>
-      <header>
-        <MainNavigation />
-      </header>
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <AllQuotes />
-          </Route>
-          <Route path="/quotes/:quoteId"><QuoteDetails /></Route>
-          <Route path="/new-quote">
-            <AddQuotes />
-          </Route>
-        </Switch>
-      </main>
-    </Fragment>
+    <Layout> {/*  Layout wrapper */}
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetails />
+        </Route>
+        <Route path="/new-quote">
+          <AddQuotes />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 

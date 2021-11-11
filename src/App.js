249a6b3f3e377cdd18ca9a4deebe626b739b85ed
unknown <kinchoily@hotmail.com>
2021-11-11 13:24:10 +0000

    App.js - setup routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { Fragment } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetails from './pages/QuoteDetails';
+import AddQuotes from './pages/AddQuotes';
+import MainNavigation from './components/layout/MainNavigation';
+
+//notes - dont forget / before the pathname
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Fragment>
+      <header>
+        <MainNavigation />
+      </header>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId"><QuoteDetails /></Route>
+          <Route path="/new-quote">
+            <AddQuotes />
+          </Route>
+        </Switch>
+      </main>
+    </Fragment>
   );
 }
 

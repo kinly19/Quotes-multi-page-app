15dc1510b62b93fc3b9516a93e3a224e5b5f17dc
unknown <kinchoily@hotmail.com>
2021-11-12 17:38:20 +0000

    QuoteList.js - update comments/notes

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -3,6 +3,15 @@ import { Fragment } from 'react';
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+// =============================== notes ===============================
+// useLocation() - returns the location object that represents the current URL,
+//  think about it like a useState that returns a new location whenever the URL changes
+
+// Query parameters are a defined set of parameters attached to the end of a url. 
+//  More simply, they are key=value pairs we can attach to a url, 
+//  used as one of many ways to pass data to an application
+//======================================================================
+
 const QuoteList = (props) => {
   return (
     <Fragment>

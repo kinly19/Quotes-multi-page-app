3bbf0c59173a01eaf01dff9f03e1c3e43e74536c
unknown <kinchoily@hotmail.com>
2021-11-15 14:10:07 +0000

    AddQuotes.js - Use custom hook for adding new quotes and handling user feedback

--- src/pages/AddQuotes.js
+++ src/pages/AddQuotes.js
@@ -1,6 +1,35 @@
+import { Fragment, useEffect} from 'react';
+import { useHistory } from 'react-router';
+import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import { addQuote } from '../lib/api';
+
+// ===================================== Notes =====================================
+// The useHistory hook gives you access to the history instance 
+//  that you may use to navigate. .push will add a new page .replace is like a redirect
+// ================================================================================= 
 
 const AddQuotes = () => {
-  return <h1>Add quotes page</h1>;
+
+  const {sendRequest, status } = useHttp(addQuote);
+
+  const history = useHistory();
+
+  useEffect(()=> {
+    if(status === 'completed'){
+      history.push('/quotes');
+    }
+  },[status, history]);
+
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
+  };
+
+  return (
+    <Fragment>
+      <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>
+    </Fragment>
+  );
 };
 
 export default AddQuotes;

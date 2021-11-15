a08581ee1242be9e28b8d1d057bb557e39555b35
unknown <kinchoily@hotmail.com>
2021-11-15 13:50:02 +0000

    Add custom hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,65 @@
+import { useReducer, useCallback } from 'react';
+
+// status notifications 
+function httpReducer(state, action) {
+  if (action.type === 'SEND') {
+    return {
+      data: null,
+      error: null,
+      status: 'pending',
+    };
+  }
+
+  if (action.type === 'SUCCESS') {
+    return {
+      data: action.responseData,
+      error: null,
+      status: 'completed',
+    };
+  }
+
+  if (action.type === 'ERROR') {
+    return {
+      data: null,
+      error: action.errorMessage,
+      status: 'completed',
+    };
+  }
+
+  return state;
+}
+
+//This hook wants a function, that can be called by this hook to send the actuial request(this will be one of the functions inside API js),
+//Also wants to know whether it should start in loading stare or not
+//Then return a function to be called to actually start sending requests
+function useHttp(requestFunction, startWithPending = false) {
+  const [httpState, dispatch] = useReducer(httpReducer, {
+    status: startWithPending ? 'pending' : null,
+    data: null,
+    error: null,
+  });
+
+  //send request
+  const sendRequest = useCallback(
+    async function (requestData) {
+      dispatch({ type: 'SEND' });
+      try {
+        const responseData = await requestFunction(requestData);
+        dispatch({ type: 'SUCCESS', responseData });
+      } catch (error) {
+        dispatch({
+          type: 'ERROR',
+          errorMessage: error.message || 'Something went wrong!',
+        });
+      }
+    },
+    [requestFunction]
+  );
+
+  return {
+    sendRequest,
+    ...httpState,
+  };
+}
+
+export default useHttp;

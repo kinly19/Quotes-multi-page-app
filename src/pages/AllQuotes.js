f965ac4623d49611fea36de01b00c97b6e1479b5
unknown <kinchoily@hotmail.com>
2021-11-15 14:03:41 +0000

    AllQuotes.js - Use custom hook to fetch and show all quotes, render conditional content for user feedback

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,25 +1,43 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
-const AllQuotes = (props) => {
+const AllQuotes = () => {
 
-  // replace with redux
-  // const DummyQuotes = [
-  //   {
-  //     id: "p1",
-  //     author: "Sarah",
-  //     text: "Learn React With React",
-  //   },
-  //   {
-  //     id: 'p2',
-  //     author: 'Emily',
-  //     text: 'Learn With Udemy'
-  //   }
-  // ];
+  //custom hook
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  //sideEffect (runs returned function inside custom hook)
+  useEffect (() => {
+    sendRequest();
+  },[sendRequest]);
+
+  //checks, return content
+  if(status === 'pending') {
+    return <div className='centered'>
+      <LoadingSpinner />
+    </div>
+  };
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>
+  };
+
+  if(status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)){
+    return <NoQuotesFound />
+  };
 
   return (
     <Fragment>
-      <QuoteList quotes={props.dummyData} />
+      <QuoteList quotes={loadedQuotes} />
     </Fragment>
   );
 };
